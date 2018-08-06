5adc54c2ce7c725f10c7a304cb372d4c610dff82
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 15:47:06 -0400

    refactored edit component and header nav component

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function EditExpensePage() {
+export default function EditExpensePage(props) {
   return (
     <div>
-      This is from Edit Expense Compy
+      Editing expense, id: {props.match.params.id}
     </div>
   );
 }

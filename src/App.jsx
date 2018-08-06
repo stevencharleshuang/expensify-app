428a215a9d41591b0ab7fb9eca07e175b16cc5cb
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 14:56:20 -0400

    added router dev basic boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from Dashboard Compy
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from Add Expense Compy
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from Edit Expense Compy
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from Help Compy
+  </div>
+);
 
 const App = () => {
   return(
     <div className="App">
-      Hello, Dave
+      <Route exact path="/" component={ExpenseDashboardPage} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('app'));

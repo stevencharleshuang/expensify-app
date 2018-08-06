a93f9b378050915bbeaf6820f5851d14ec0a9f43
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 14:22:50 -0400

    created webpack boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-// import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
 const App = () => {
   return(
-    <IndecisionApp />
+    <div className="App">
+    </div>
   );
 }
 

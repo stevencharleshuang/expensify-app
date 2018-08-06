6cfd4ef04d42d6d01a5db910beba26fde140965d
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 15:10:32 -0400

    addded nav functionality and basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, NavLink} from 'react-router-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
@@ -31,12 +31,25 @@ const HelpPage = () => (
 const NotFoundPage = () => (
   <div>
     404!
+    <br /><br />
+    <Link to="/">Go Home</Link>
   </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact> Dashboard </NavLink>
+    <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
+    <NavLink to="/edit" activeClassName="is-active"> Edit Expense </NavLink>
+    <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
+  </header>
 );
 
 const App = () => {
   return(
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={ExpenseDashboardPage} />
         <Route path="/create" component={AddExpensePage} />

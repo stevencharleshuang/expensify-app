a85cd357135c1e70f7bab9c2700002e774e25cff
Steven Huang <steven.charles.huang@gmail.com>
2018-08-07 18:22:49 -0400

    added calendar and functionality for date selection

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     amount: '',
+    createdAt: moment(),
+    calendarFocused: false,
     note: '',
   };
 
@@ -17,6 +23,14 @@ export default class ExpenseForm extends React.Component {
     if (amount.match(/^\d*(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
+  }
+
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  }
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }))
   }
 
   onNoteChange = (e) => {
@@ -40,6 +54,14 @@ export default class ExpenseForm extends React.Component {
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expenses (optional)"

e4b59eb6c93fe5153af761058db2d69ca84380bb
Steven Huang <steven.charles.huang@gmail.com>
2018-08-07 18:42:52 -0400

    added adding data to redux store functionality and redirect to dashboard on form submit

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-export default function AddExpensePage() {
+const AddExpensePage = (props) => {
   return (
     <div>
       <h1>Add Expense</h1>
       <ExpenseForm
         onSubmit={(expense) => {
-          console.log(expense);
+          props.dispatch(addExpense(expense));
+          props.history.push('/');
         }}
       />
     </div>
   );
 }
+
+export default connect()(AddExpensePage);

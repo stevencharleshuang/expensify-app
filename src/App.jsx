1ee86310ecb9c8f8db27914424eeae7ea4143970
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 22:07:16 -0400

    set up filetree to connect redux to react app, separated concerns from redux file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+
+console.log(visibleExpenses);
 
 const App = () => {
   return(
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  // entry: './src/App.jsx',
-  entry: './src/sandbox/redux-expensify.js',
+  entry: './src/App.jsx',
+  // entry: './src/sandbox/redux-expensify.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'

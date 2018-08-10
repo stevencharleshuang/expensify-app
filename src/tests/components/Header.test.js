8ada90005f9d17e5657adb6eff1e40fc03f76a75
Steven Huang <steven.charles.huang@gmail.com>
2018-08-10 18:56:48 -0400

    addded enzyme toJSON pkg and refactored Header test

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json'
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  // console.log(renderer.getRenderOutput());
-  expect(renderer.getRenderOutput())
-    .toMatchSnapshot();
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // // console.log(renderer.getRenderOutput());
+  // expect(renderer.getRenderOutput())
+  //   .toMatchSnapshot();
+
+  // Enzyme Refactor:
+
+  const wrapper = shallow(<Header />);
+  // expect(wrapper.find('h1').length).toBe(1);
+  // expect(wrapper.find('h1').text()).toBe('Expensify App');
+  // expect(wrapper).toMatchSnapshot();
+
+  // Enzyme toJSON Refactor
+
+  expect(toJSON(wrapper)).toMatchSnapshot();
 })

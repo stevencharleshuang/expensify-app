67e75917fb31d9d8be92698639191dc585b25fdf
Steven Huang <steven.charles.huang@gmail.com>
2018-08-10 18:00:49 -0400

    set up tests and tested reducers/expenses

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -1,7 +1,7 @@
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 
-const expensesReducer = (state = [], action) => {
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_EXPENSE':
       return [
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,25 +1,6 @@
 import moment from 'moment'
 import selectExpenses from '../../selectors/expenses';
-
-const expenses = [{
-  id: '1',
-  description: 'gum',
-  note: '',
-  amount: 195,
-  createdAt: 0
-}, {
-  id: '2',
-  description: 'rent',
-  note: '',
-  amount: 1078900,
-  createdAt: moment(0).subtract(4, 'days').valueOf()
-}, {
-  id: '3',
-  description: 'credit card',
-  note: '',
-  amount: 1030,
-  createdAt: moment(0).add(4, 'days').valueOf()
-}];
+import expenses from '../fixtures/expenses';
 
 test('should filter by text value', () => {
   const filters = {

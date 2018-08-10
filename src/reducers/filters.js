ea5b8fb0304f41e843723a27ae256f28d29de816
Steven Huang <steven.charles.huang@gmail.com>
2018-08-10 17:15:34 -0400

    set up tests and tested reducers/filters

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,27 +1,29 @@
+import moment from 'moment';
+
 // Filters Reducer
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
-    case 'SET_TEXT_FILTER':
-      return {
-        ...state,
-        text: action.text
-      }
-    case 'SORT_BY-AMOUNT':
+    case 'SORT_BY_AMOUNT':
       return {
         ...state,
         sortBy: 'amount'
       }
-    case 'SORT_BY-DATE':
+    case 'SORT_BY_DATE':
       return {
         ...state,
         sortBy: 'date'
+      }
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
       }
     case 'SET_START_DATE':
       return {

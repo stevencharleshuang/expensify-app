7f648c15358b8091306e13a11ede34623f731137
Steven Huang <steven.charles.huang@gmail.com>
2018-08-14 21:36:09 -0400

    refactored edit expense page

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,14 +5,14 @@ import toJSON from 'enzyme-to-json'
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import expenses from '../fixtures/expenses';
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 
 beforeEach(() => {
-  onSubmit = jest.fn();
+  addExpense = jest.fn();
   history = { push: jest.fn() }
   wrapper = shallow(
     <AddExpensePage
-      onSubmit={onSubmit}
+      addExpense={addExpense}
       history={history}
     />
   );
@@ -22,8 +22,8 @@ test('should render AddExpensePage with expenses', () => {
   expect(toJSON(wrapper)).toMatchSnapshot();
 });
 
-test('should handle onSubmit', () => {
+test('should handle addExpense', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
